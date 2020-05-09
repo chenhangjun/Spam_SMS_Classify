b3aa44a719e146f3f1d1a1a70dc9466c40d3ee7d
chenhangjun <976913999@qq.com>
2020-05-09 17:38:50 +0800

    主界面

--- MainPage.py
+++ MainPage.py
@@ -4,109 +4,125 @@ import math
 from zhon.hanzi import punctuation
 import jieba
 
-def ReadModel(filename):
 
-  with open('modeldict.json') as f:
-    dictObj = json.load(f)
-    ham_map = dictObj['ham']
-    spam_map = dictObj['spam']
+class MainPage(object):
 
-  return ham_map, spam_map
+  def __init__(self, master=None):
 
-
-def Predict(text):
-
-  ham_map, spam_map = ReadModel('modeldict.json')
-  ham_words_count = 323632
-  spam_words_count = 97890
-  ham_count = 35978
-  spam_count = 4022
-  words_set_size = 62133
-  para = 0.4743
-
-  ham_probability = ham_count / (ham_count + spam_count)
-  spam_probability = spam_count / (ham_count + spam_count)
-
-  ham_pro = 0.0
-  spam_pro = 0.0
-
-  for word in text:
-    ham_pro += math.log((ham_map.get(word, 0) + 1) / (ham_words_count + words_set_size))
-    spam_pro += math.log((spam_map.get(word, 0) + 1) / (spam_words_count + words_set_size))
-
-  ham_pro += math.log(ham_probability)
-  spam_pro += math.log(spam_probability)
-
-  # 1为spam, 0为ham，与标签一致
-  # return int(spam_pro >= ham_pro)
-  tot = spam_pro + ham_pro
-  threshold = tot * para
-  if spam_pro >= threshold:
-    return 1
-  else:
-    return 0
+    self.content = ''
+    self.result = ''
+    self.root = master  # 定义内部变量root
+    self.root = Tk(className='垃圾短信识别')  # 窗口标题
+    self.root.geometry('%dx%d' % (600, 400))  # 设置窗口大小
+    self.root.resizable(0, 0)
+    # 输入框
+    self.text_1 = Text(self.root, width=62, height=12)
+    # label 3 显示结果
+    self.label_3 = Label(self.root)
+    self.createPage()
+    self.ham_map, self.spam_map = self.ReadModel('modeldict.json')
 
 
-# 定义按钮点击事件
-def on_click(input, label):
+  def createPage(self):
 
-  # 获取输入内容
-  sentence = input.get('0.0', 'end')
-  # 分词
-  stop_words = ['有', '和', '是', '在', '我', '了', '的']
-  remove_chars = '[0-9’a-zA-Z!"#$%&\'()*\\\\+,-./:;<=>?@?★…‘’[\\]^_`{|}~(\s*)]+'
-  newstr = re.sub(remove_chars, '', sentence)
-  sentence = re.sub("[{}]+".format(punctuation), "", newstr)
-  list_words = jieba.lcut(sentence)
-  for word in list_words:
-    if word in stop_words:
-      list_words.remove(word)
+    # 添加一个label
+    self.page = Frame(self.root)  # 创建Frame
+    self.page.pack()
+    Label(self.page).grid(row=0, stick=W)
 
-  # 预测
-  tag = Predict(list_words)
-  # 修改标签显示
-  if tag == 1:
-    label['text'] = '垃圾邮件'
-  else:
-    label['text'] = '正常邮件'
+    '''
+    Label(self.page, text='输入短信').grid(row=1, column=1, stick=W)
+    Entry(self.page, textvariable=self.content, width=70).grid(row=3, column=1)
+    Label(self.page, text='判定结果为：').grid(row=5, stick=W, pady=10)
+    Label(self.page, text=self.result).grid(row=5, stick=W, pady=10)
+    Button(self.page, text='判定', command=self.Judge).grid(row=5, column=7, stick=W)
+    '''
+    # 添加一个label
+    label_1 = Label(self.root)
+    label_1['text'] = '输入短信:'
+    label_1.place(x=50, y=50)
 
+    self.text_1.place(x=50, y=90)
 
-def main():
-
-  root = Tk(className = '垃圾短信识别')   # 窗口标题
-  root.geometry('600x400')
-  root.resizable(0, 0)
+    # label 2
+    label_2 = Label(self.root)
+    label_2['text'] = '判定结果为:'
+    label_2.place(x=50, y=320)
 
 
-  # 添加一个label
-  label_1 = Label(root)
-  label_1['text'] = '输入短信:'
-  label_1.place(x = 30, y = 50)
+    self.label_3.place(x=130, y=320)
 
-  # 输入框
-  # text_1 = StringVar()
-  # entry_1 = Entry(root)
-  # entry_1['textvariable'] = text_1
-  # entry_1.place(x = 50, y = 80, width = 500, height = 200)
-  text_1 = Text(root, width = 62, height = 12)
-  text_1.place(x = 50, y = 90)
-
-  # label 2
-  label_2 = Label(root)
-  label_2['text'] = '判定结果为:'
-  label_2.place(x = 50, y = 320)
-
-  # label 3 显示结果
-  label_3 = Label(root)
-  # label_3['text'] = '垃圾邮件'
-  label_3.place(x = 130, y = 320)
-
-  # button
-  button = Button(root, text = '判定', command = lambda: on_click(text_1, label_3))
-  button.place(x = 500, y = 310)
-
-  root.mainloop()
+    # button
+    button = Button(self.root, text='判定', command=self.Judge)
+    button.place(x=500, y=310)
 
 
-if __name__ == "__main__":
-  main()
+  def Judge(self):
+
+    # 获取输入内容
+    sentence = self.text_1.get('0.0', 'end')
+    # 分词
+    stop_words = ['有', '和', '是', '在', '我', '了', '的']
+    remove_chars = '[0-9’a-zA-Z!"#$%&\'()*\\\\+,-./:;<=>?@?★…‘’[\\]^_`{|}~(\s*)]+'
+    newstr = re.sub(remove_chars, '', sentence)
+    sentence = re.sub("[{}]+".format(punctuation), "", newstr)
+    list_words = jieba.lcut(sentence)
+    for word in list_words:
+      if word in stop_words:
+        list_words.remove(word)
+
+    # 预测
+    tag = self.Predict(list_words)
+    # 修改标签显示
+    if tag == 1:
+      self.result = '垃圾邮件'
+    else:
+      self.result = '正常邮件'
+
+    self.label_3['text'] = self.result
+
+
+  def Predict(self, text):
+
+
+    ham_words_count = 323632
+    spam_words_count = 97890
+    ham_count = 35978
+    spam_count = 4022
+    words_set_size = 62133
+    para = 0.4743
+
+    ham_probability = ham_count / (ham_count + spam_count)
+    spam_probability = spam_count / (ham_count + spam_count)
+
+    ham_pro = 0.0
+    spam_pro = 0.0
+
+    for word in text:
+      ham_pro += math.log((self.ham_map.get(word, 0) + 1) / (ham_words_count + words_set_size))
+      spam_pro += math.log((self.spam_map.get(word, 0) + 1) / (spam_words_count + words_set_size))
+
+    ham_pro += math.log(ham_probability)
+    spam_pro += math.log(spam_probability)
+
+    # 1为spam, 0为ham，与标签一致
+    # return int(spam_pro >= ham_pro)
+    tot = spam_pro + ham_pro
+    threshold = tot * para
+    if spam_pro >= threshold:
+      return 1
+    else:
+      return 0
+
+
+  def ReadModel(self, filename):
+
+    with open(filename) as f:
+      dictObj = json.load(f)
+      ham_map = dictObj['ham']
+      spam_map = dictObj['spam']
+
+    return ham_map, spam_map
+
+
+
